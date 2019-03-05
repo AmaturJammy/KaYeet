93acacab2f1b92e0f80047f8875befa30b4ff7c4
Yorick01 <yorick.vancampen@student.aventus.nl>
2019-03-05 16:14:00 +0100

    main

---
+++ Website/Main/main.php
@@ -0,0 +1,13 @@
+<?<!DOCTYPE html>
+<html>
+<head>
+	<title>KaYeet</title>
+	<link href='https://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet'>
+	<link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+<h1 id="pageTitle">
+	KaYeet
+</h1>
+</body>
+</html>
