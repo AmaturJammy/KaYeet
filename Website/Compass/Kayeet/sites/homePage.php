2d9698d341df1b494350409930be92e63d779ccb
arjenvklaveren <arjenvklaveren@gmail.com>
2019-03-19 16:35:46 +0100

    project overgezet naar compass en scss

---
+++ Website/Compass/Kayeet/sites/homePage.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+	<title>KaYeet home</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href='https://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet'>
+	<link rel="stylesheet" type="text/css" href="../stylesheets/style.css">
+</head>
+<body>
+    <div id="titleTextDiv">
+        <h1 id="pageTitle">
+            KaYeet
+        </h1>
+    </div>
+    <div id="contentsDiv">
+        <button id="joinButton">Join</button><br>
+        <button id="loginButton">login</button>
+    </div>
+</body>
+</html>
