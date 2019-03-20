22d4a15ae34f82ac0bfe0f1c2c7fe64ea2cc9b5c
arjenvklaveren <arjenvklaveren@gmail.com>
2019-03-20 09:40:26 +0100

    Register pagina frontend

--- Website/Compass/Kayeet/sites/registerPage.php
+++ Website/Compass/Kayeet/sites/registerPage.php
@@ -13,11 +13,15 @@
         </h1>
     </div>
     <div id="contentsDiv">
-        <input type="text" id="usernameInputfield"><br/>
-        <input type="text" id="emailInputfield"><br/>
-        <input type="text" id="passwordInputfield1"><br/>
-        <input type="text" id="passwordInputfield2"><br/>
-        <button id="defaultButton">Register</button>
+         <form method="post">
+            <input type="text" placeholder="Gebruikersnaam" id="usernameInputfield" name="screenName" <?php if(isset($screenName)){echo('value='.$screenName);}?>><br>
+            <input type="email" placeholder="E-Mail" id="emailInputfield" name="email" <?php if(isset($email)){echo('value='.$email);}?>><br>
+            <input type="password"  placeholder="Wachtwoord" id="passwordInputfield1" name="password"><br>
+            <input type="password"  placeholder="Bevestig WW" id="passwordInputfield2" name="confirmPassword"><br>
+            <input type="submit" id="defaultButton" name="submitRegistration" value="Registreren">
+        </form>
     </div>
 </body>
 </html>
+
+
