f0d07099cbc3784773353de3c8c675a08fa2b451
arjenvklaveren <arjenvklaveren@gmail.com>
2019-04-09 15:10:53 +0200

    frontend

--- Website/Compass/Kayeet/sites/loginPage.php
+++ Website/Compass/Kayeet/sites/loginPage.php
@@ -1,25 +1,27 @@
-
 <html>
-<head>
-	<title>KaYeet login</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link href='https://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet'>
-	<link rel="stylesheet" type="text/css" href="../stylesheets/style.css">
-</head>
-<body>
-    <div id="titleTextDiv">
-        <h1 id="pageTitle">
-            KaYeet
-        </h1>
-    </div>
-    <div id="contentsDiv">
-        <form method="post">
-            <input type="email" placeholder="E-Mail:" class="defaultInputfield" name="email"><br>
-            <input type="password" placeholder="Wachtwoord:" class="defaultInputfield" name="password"><br>
-            <input type="submit" class="defaultButton" name="submitinlog" value="Login">
+    <head>
+        <title>KaYeet</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href='https://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet'>
+        <link rel="stylesheet" type="text/css" href="../stylesheets/stylesheet.css">
+    </head>
+    <body>
+        <div id="titleDiv">
+            <h1 id="pageTitle">
+                KaYeet
+            </h1>
+        </div>
+        <form>
+            <div id="contentsDiv">
+                <input required='' type='text'>
+                <label alt='Email' placeholder='Typ je e-mail'></label>
+                <input class="password" required='' type='password'>
+                <label alt='Wachtwoord' placeholder='Verzin een wachtwoord'></label>
+            </div>
+            <div class="buttonDiv">
+                <button class="btn-hover color-1">Registreren</button>
+                <input type="submit" class="btn-hover color-1" value="Inloggen">
+            </div>
         </form>
-    </div>
-</body>
+    </body>
 </html>
-
-
--- Website/Compass/Kayeet/sites/registerPage.php
+++ Website/Compass/Kayeet/sites/registerPage.php
@@ -1,27 +1,32 @@
 
 <html>
-<head>
-	<title>KaYeet register</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link href='https://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet'>
-	<link rel="stylesheet" type="text/css" href="../stylesheets/style.css">
-</head>
-<body>
-    <div id="titleTextDiv">
-        <h1 id="pageTitle">
-            KaYeet
-        </h1>
-    </div>
-    <div id="contentsDiv">
-         <form method="post">
-            <input type="text" required placeholder="Naam:" class="defaultInputfield" name="screenName" <?php if(isset($screenName)){echo('value='.$screenName);}?>><br>
-            <input type="email" required placeholder="E-Mail:" class="defaultInputfield" name="email" <?php if(isset($email)){echo('value='.$email);}?>><br>
-            <input type="password" required placeholder="Wachtwoord:" class="defaultInputfield" name="password"><br>
-            <input type="password" required placeholder="Bevestig WW:" class="defaultInputfield" name="confirmPassword"><br>
-            <input type="submit" required class="defaultButton" name="submitRegistration" value="Registreer">
-        </form>
-    </div>
-</body>
+    <head>
+        <title>KaYeet</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href='https://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet'>
+        <link rel="stylesheet" type="text/css" href="../stylesheets/stylesheet.css">
+    </head>
+    <body>
+        <div id="titleDiv">
+            <h1 id="pageTitle">
+                KaYeet
+            </h1>
+        </div>
+        <form method="post">
+        <div id="contentsDiv">
+                <input required='' type='text'>
+                <label alt='Gebruiksnaam' placeholder='Kies een gebruiksnaam' id="usernameInputfield" name="screenName" <?php if(isset($screenName)){echo('value='.$screenName);}?>></label>
+                <input required='' type='text'>
+                <label alt='Email' placeholder='Typ je e-mail' id="emailInputfield" name="email" <?php if(isset($email)){echo('value='.$email);}?>></label>
+                <input class="password" required='' type='password'>
+                <label alt='Wachtwoord' placeholder='Verzin een wachtwoord'></label>
+                <input class="password" required='' type='password'>
+                <label alt='Herhaal wachtwoord' placeholder='Herhaal je wachtwoord'></label>
+        </div>
+        <div class="buttonDiv">
+                <input type="submit" class="btn-hover color-1" name="submitRegistration" value="Registreren">
+                <button class="btn-hover color-1">Login?</button> 
+        </div>
+    </form>
+    </body>
 </html>
-
-
