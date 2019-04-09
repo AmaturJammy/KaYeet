dc0c0173beb2e7c9015dd5b79aa9f79153d44a66
arjenvklaveren <arjenvklaveren@gmail.com>
2019-04-09 18:06:08 +0200

    scorepage

--- Website/Compass/Kayeet/sites/homePage.php
+++ Website/Compass/Kayeet/sites/homePage.php
@@ -3,7 +3,7 @@
         <title>KaYeet home</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href='https://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet'>
-        <link rel="stylesheet" type="text/css" href="../stylesheets/style.css">
+        <link rel="stylesheet" type="text/css" href="../stylesheets/stylesheet.css">
     </head>
     <body>
         <div id="titleTextDiv">
