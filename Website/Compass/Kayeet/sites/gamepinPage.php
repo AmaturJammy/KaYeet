a37807fc37d4c4118e4c508909004ff16299ddad
arjenvklaveren <arjenvklaveren@gmail.com>
2019-04-09 18:17:14 +0200

    scorePageCommit

---
+++ Website/Compass/Kayeet/sites/gamepinPage.php
@@ -0,0 +1,25 @@
+
+<html>
+    <head>
+        <title>KaYeet</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href='https://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet'>
+        <link rel="stylesheet" type="text/css" href="../stylesheets/stylesheet.css">
+    </head>
+    <body>
+        <div id="titleDiv">
+            <h1 id="pageTitle">
+                KaYeet
+            </h1>
+        </div>      
+        <div id="contentsDiv">
+            <form>
+                <input required='' type='text'>
+                <label alt='Gamepin' placeholder='Voer de gamepin in'></label>
+                <input type="submit" class="btn-hover color-1" id="gamePinButton" value="Join">
+            </form>
+            
+        </div>
+        <button onclick="location.href='homePage.php'" id="loginregistreer" class="btn-hover color-1">Login/Registreer</button>
+    </body>
+</html>
